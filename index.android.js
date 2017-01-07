731df7643c33da9d245f98b4f1f5849650519e1a
Iván Portilla <jcuastumal@gmail.com>
2017-01-07 13:35:00 -0500

    Project base deleted since commit ecb67f5f.

--- index.android.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import React, { AppRegistry } from 'react-native';
-
-import Application from './Application';
-AppRegistry.registerComponent('feathersreactnativechat', () => Application);
--- index.ios.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-import React, { AppRegistry } from 'react-native';
-
-import Application from './Application';
-AppRegistry.registerComponent('feathersreactnativechat', () => Application);
